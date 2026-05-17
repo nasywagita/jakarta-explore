ecaf604d511fb68c018f1a0e3a26142f4c1f6f6b
nasywagita <nasywaamelia14@gmail.com>
2026-05-17 16:01:58 +0700

    merapikan kode dan merombak tampilan menjadi responsif

--- lib/wisata_screen.dart
+++ lib/wisata_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
+
 import 'core/theme/app_colors.dart';
 import 'core/theme/app_text_styles.dart';
 import 'providers/destinations_provider.dart';
